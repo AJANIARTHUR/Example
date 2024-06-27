2c9c66a707dce347242298aa1ae8ce8a8c9e1be8
Admin <admin@TheHiddsProject.lan>
2024-06-27 11:08:41 -0700

    saving file

---
+++ sum_example.py
@@ -0,0 +1,8 @@
+# The function is starting at 5 and counting dowm to one.
+def sum(num):
+    result=0
+    for i in range(num):
+        result + result = num -1
+    print(result)
+        
+sum(5)
